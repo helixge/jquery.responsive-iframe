59079c7bb62f3dfb73d6e4669712eb883aeda20f
Thor <jangveladze.thornike@gmail.com>
2017-09-20 16:27:52 +0400

    Refactored ration option calculation
    Minification was causing problems.

--- src/jquery.responsive-iframe.js
+++ src/jquery.responsive-iframe.js
@@ -1,7 +1,8 @@
 $.fn.responsiveIframe = function(options) {
   var _this = this;
   var _randomClassName = "";
-
+  var _options = null;
+  
   var process = function() {
     _this.each(function() {
       $(this).wrap('<div class="' + _randomClassName + '"></div>');
@@ -10,14 +11,15 @@ $.fn.responsiveIframe = function(options) {
 
   var createClasses = function() {
     _randomClassName = "responsiveiframe-" + parseInt(Math.random() * 1000000) + "-" + parseInt(Math.random() * 1000000);
-    var styleCss = "." + _randomClassName + " { position: relative; padding-bottom: 56.25%; padding-top: 25px; height: 0;} ." + _randomClassName + " iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }";
+    var styleCss = "." + _randomClassName + " { position: relative; padding-bottom: " + _options.ratio + "%; padding-top: 25px; height: 0;} ." + _randomClassName + " iframe { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }";
     var styleElem = document.createElement("style");
     styleElem.innerHTML = styleCss;
     document.head.appendChild(styleElem);
   };
 
-  var _options = options || {};
-  _options.ratio = (options.ratio || (9 / 16)) * 100;
+  _options = options || {};
+  var ratio = options.ratio || (9 / 16);
+  _options.ratio = ratio * 100;
 
   createClasses();
   setTimeout(process());
